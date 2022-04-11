b25b1fb931f3b995f49749342cc642413a3a8668
Shamim <yeasinshamim7@gmail.com>
2022-04-11 16:16:09 +0600

    responsive done

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({ service }) => {
+    const { name, img, description, price } = service;
+    return (
+        <div className='service-container'>
+            <div className='service-img'>
+                <img src={img} alt="Car services" />
+            </div>
+            <div className='service-info'>
+                <h2>{name}</h2>
+                <p>Price: {price}</p>
+                <p><small>{description}</small></p>
+                <button>Book: {name}</button>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
 import './Services.css';
 
 const Services = () => {
@@ -12,7 +13,15 @@ const Services = () => {
 
     return (
         <div>
-            <h4>Services: {services.length}</h4>
+            <h1 className='services-title'>Our Services</h1>
+            <div className='services-container'>
+                {
+                    services.map(service => <Service
+                        key={service.id}
+                        service={service}
+                    />)
+                }
+            </div>
         </div>
     );
 };
