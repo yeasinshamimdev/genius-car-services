19e8f3ff39f832d00c4f4e9171814f9304499cb0
Shamim <yeasinshamim7@gmail.com>
2022-04-11 08:23:23 +0600

    basic setup init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header />
+      <Routes>
+        <Route path='/' />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     const date = new Date();
     const year = date.getFullYear();
     return (
         <footer>
-            <p><small>copyright © {year}</small></p>
+            <p><small>copyright © {year} || Yeasin Shamim</small></p>
         </footer>
     );
 };
