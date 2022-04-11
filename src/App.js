e7e98a9ac23724176109cd641a79124bb9d32944
Shamim <yeasinshamim7@gmail.com>
2022-04-11 15:20:42 +0600

    services loaded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home/Home';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -8,7 +10,8 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
       <Footer />
     </div>
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Services.css';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, []);
+
     return (
         <div>
-            <h3>this is Services component</h3>
+            <h4>Services: {services.length}</h4>
         </div>
     );
 };
